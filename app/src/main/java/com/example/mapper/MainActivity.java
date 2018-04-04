438b6c00052bdc8b7dd35123c84d2e7c77d6fc4a
Calvin Ly <calvinly@utexas.edu>
2018-04-04 00:12:57 -0500

    fix text box to search on enter

--- app/src/main/java/com/example/mapper/MainActivity.java
+++ app/src/main/java/com/example/mapper/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.mapper;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_LOCATION = "com.example.myfirstapp.LOCATION";
@@ -13,6 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText editText = (EditText) findViewById(R.id.edit_location);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    searchLocation(v);
+                    handled = true;
+                }
+                return handled;
+            }
+        });
     }
 
     public void searchLocation(View view) {
